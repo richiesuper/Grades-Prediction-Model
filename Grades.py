527a9eb738c122840d6a7031cc3fbe3cafba84b7
Mikask <darrenprasetya40@gmail.com>
2021-08-02 19:10:53 +0800

    show correlation between pretest and posttest

--- Grades.py
+++ Grades.py
@@ -2,4 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-original_df = pd.read_csv('test_scores.csv')
+import matplotlib.pyplot as plt
+df = pd.read_csv('test_scores.csv')
+
+
+def show_correlation():
+    test_results = pd.DataFrame({'Pretest': df['pretest'], 'Posttest': df['posttest']})
+    test_results.plot()
+    plt.show()
+
+show_correlation()
